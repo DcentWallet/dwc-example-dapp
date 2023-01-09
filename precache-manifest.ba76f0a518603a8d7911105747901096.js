8b2b800f07998f44095cbf4cdcd6c376cc7d4d48
SeongHo-Won <sh.won@iotrust.kr>
2023-01-09 15:45:55 +0900

    Updates

---
+++ precache-manifest.ba76f0a518603a8d7911105747901096.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff20b8abf6659cbab77",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/js/main.aff20b8a.chunk.js"
+  },
+  {
+    "revision": "6472d08c914503d74fad",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/js/1.6472d08c.chunk.js"
+  },
+  {
+    "revision": "5d766843f323075103d9",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/js/runtime~main.5d766843.js"
+  },
+  {
+    "revision": "d46849e695c8a415f7c71d4b21feadce",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/media/erc20.d46849e6.svg"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "5d1692b26ef29ea7b4e3722e1fc854b5",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/media/eth.5d1692b2.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/DcentWallet/dwc-example-dapp.git/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "5ef98b9e139f2e22ddb403b8a577c3d0",
+    "url": "/DcentWallet/dwc-example-dapp.git/index.html"
+  }
+];
